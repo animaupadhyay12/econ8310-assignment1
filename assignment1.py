79bf38e2b28795844cf1b76aea629c38ea8adf0a
Anima <anima.upadhyay23@gmail.com>
2025-02-17 15:41:39 -0600

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -8,20 +8,28 @@ Original file is located at
 """
 
 import pandas as pd
-import statsmodels.api as sm
 
 train = pd.read_csv('/assignment_data_train.csv')
 test = pd.read_csv('/assignment_data_test.csv')
+
 train.head()
+
+
+
+import statsmodels.api as sm
+
 
 x = train.drop(['trips', 'Timestamp'], axis = 1)
 y = train[['trips']]
-
 model = sm.OLS(endog = y, exog = x)
-
 modelFit = model.fit()
 
+# model = sm.OLS(endog = y, exog = x)
+# modelFit = model.fit()
+
 x_test = test.drop(['Timestamp'], axis = 1)
+
 pred = modelFit.predict(x_test)
+
 print(modelFit.summary())
 
